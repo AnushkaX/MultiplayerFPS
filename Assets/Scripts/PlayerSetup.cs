a15153d803b8533f5307580df7de285c12bd45fd
AnushkaX <anushka.mutusinghe@gmail.com>
2021-06-16 15:26:52 +0530

    2 players added

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentToDisable;
+
+    Camera sceneCamera;
+
+    private void Start()
+    {
+        if(!isLocalPlayer)
+        {
+            for(int i = 0; i< componentToDisable.Length; i++)
+            {
+                componentToDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+
+            if(sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+            
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if(sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
